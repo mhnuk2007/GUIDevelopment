b7c39a7030a57f78a1c0968e5164bb5bfe4b36dc
Mohan Lal <mhnuk2007@gmail.com>
2025-06-14 17:20:14 +0500

    Add LableButtonExample.java

---
+++ src/Swing/LableButtonExample.java
@@ -0,0 +1,22 @@
+package Swing;
+
+import javax.swing.*;
+
+public class LableButtonExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lable and Button Example");
+        JPanel panel = new JPanel(); // container for components
+
+        JLabel label = new JLabel("Click the button");
+        JButton button = new JButton("Click Me!");
+
+        panel.add(label); // add components to panel
+        panel.add(button);
+
+        frame.add(panel); // add panel to frame
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
