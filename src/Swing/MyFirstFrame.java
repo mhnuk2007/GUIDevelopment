6a62811e83998c6bdd40eff72f447d5d86d3d566
Mohan Lal <mhnuk2007@gmail.com>
2025-06-14 17:08:31 +0500

    Initial commit: Setup GUI project with MyFirstFrame.java

---
+++ src/Swing/MyFirstFrame.java
@@ -0,0 +1,13 @@
+package Swing;
+
+import javax.swing.*;
+
+public class MyFirstFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My first frame!");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
